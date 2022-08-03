df91a1c684552d1776bdfc946785e910e2d82818
Leonomics <leocv97@gmail.com>
2022-08-03 15:56:47 +0200

    add objects array

---
+++ myscript.js
@@ -0,0 +1,47 @@
+let teamMember = {
+    userName,
+    userRole,
+    userImage
+}
+
+let teamMember1 = teamMember;
+let teamMember2 = teamMember;
+let teamMember3 = teamMember;
+let teamMember4 = teamMember;
+let teamMember5 = teamMember;
+let teamMember6 = teamMember;
+
+console.log(teamMember1)
+
+let teamMembers = [
+    {
+        userName: "Wayne Barnett",
+        userRole: "Founder & CEO",
+        userImage:  "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        userName: "Angela Caroll",
+        userRole: "Chief Editor",
+        userImage:  "angela-caroll-chief-editor.jpg"
+    },
+    {
+        userName: "Walter Gordon",
+        userRole: "Office Manager",
+        userImage:  "walter-gordon-office-manager.jpg"
+    },
+    {
+        userName: "Angela Lopez",
+        userRole: "Social Media Manager",
+        userImage:  "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        userName: "Scott Estrada",
+        userRole: "Developer",
+        userImage:  "scott-estrada-developer.jpg"
+    },
+    {
+        userName: "Barbara Ramos",
+        userRole: "Graphic Designer",
+        userImage:  "barbara-ramos-graphic-designer.jpg"
+    }
+]
